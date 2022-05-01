6ded80a6306b2db16669b425b72fd5b20a6417cd
M <82416283+max-hertz@users.noreply.github.com>
2022-05-01 17:52:42 +0200

    integrated prisma client, createKita() method

--- smartkita-backend/controllers/kitasController.js
+++ smartkita-backend/controllers/kitasController.js
@@ -1,4 +1,4 @@
-// TODO: require model (prisma)
+const prisma = require('../lib/prisma.js');
 
 exports.kitasList = (req, res) => {
     res.send('NOT IMPLEMENTED: kitas list')
@@ -7,3 +7,21 @@ exports.kitasList = (req, res) => {
 exports.getKitaById = (req, res) => {
     res.send('NOT IMPLEMENTED: get a single kita')
 };
+
+exports.createKita = async (req, res) => {
+    // TODO: error handling
+    const { smartcity_teilnehmer, name, traeger, strasse, hausnr, plz, traeger_kategorie, kurzbeschreibung } = req.body
+    const post = await prisma.einrichtung.create({
+        data: {
+            smartcity_teilnehmer,
+            name,
+            traeger,
+            strasse,
+            hausnr,
+            plz,
+            traeger_kategorie,
+            kurzbeschreibung
+        },
+    })
+    res.json(post)
+}
--- smartkita-backend/routes/kitas.js
+++ smartkita-backend/routes/kitas.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const kitasController = require('../controllers/kitasController.js');
 
 // GET all kitas' data
@@ -7,5 +7,8 @@ router.get('/', kitasController.kitasList);
 
 // GET a kita by id
 router.get('/:id', kitasController.getKitaById);
+
+// POST a new kita
+router.post('/post', kitasController.createKita);
 
 module.exports = router;
