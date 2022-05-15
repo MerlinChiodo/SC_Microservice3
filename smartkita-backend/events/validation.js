abef2911ea91a8381617fd3ec5a4c7f6f9f8a3ac
M <82416283+max-hertz@users.noreply.github.com>
2022-05-15 17:22:08 +0200

    added landingpage event

--- smartkita-backend/events/validation.js
+++ smartkita-backend/events/validation.js
@@ -4,7 +4,8 @@ const Ajv = require('ajv');
 const schema_sendTicketInquiry = require('./stadtbus/sendTicketInquirySchema.json');
 const schema_receiveRefugeeKitaApplication = require('./integration/receiveRefugeeKitaApplicationSchema.json');
 const schema_sendCalendarEntry = require('./forum/sendCalendarEntrySchema.json');
-const schema_sendNewsletterPost = require('./forum/sendNewsletterPostSchema.json')
+const schema_sendNewsletterPost = require('./forum/sendNewsletterPostSchema.json');
+const schema_sendChangeAboutUs = require('./landingpage/sendChangeAboutUsSchema.json');
 
 // what's happening here?
 const ajv = exports.ajv = new Ajv();
@@ -13,3 +14,4 @@ ajv.addSchema(schema_sendTicketInquiry, 'stadtbus_sendTicketInquiry');
 ajv.addSchema(schema_receiveRefugeeKitaApplication, 'integration_receiveRefugeeKitaApplication');
 ajv.addSchema(schema_sendCalendarEntry, 'forum_sendCalendarEntry');
 ajv.addSchema(schema_sendNewsletterPost, 'forum_sendNewsletterPost');
+ajv.addSchema(schema_sendChangeAboutUs, 'landingpage_sendChangeAboutUs');
