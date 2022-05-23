fd970e38321183223252a79517e08e7da1a99407
M <82416283+max-hertz@users.noreply.github.com>
2022-05-23 13:19:07 +0200

    another fix for frontend deploy

--- smartkita-backend/app.js
+++ smartkita-backend/app.js
@@ -68,10 +68,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
-app.use(express.static(path.join(__dirname, "../smartkita-frontend",'dist')));
+app.use(express.static(path.join(__dirname, "../frontend",'dist')));
 
 app.get("*", async (req, res) =>{
-  res.sendFile(path.join(__dirname, "../smartkita-frontend","dist", "index.html"))
+  res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
 })
+
 module.exports = app;
