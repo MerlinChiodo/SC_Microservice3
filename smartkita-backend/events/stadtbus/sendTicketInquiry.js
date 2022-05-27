47f2dfdcf21146c73e4b6c4fd9d5ca7d21352e76
M <82416283+max-hertz@users.noreply.github.com>
2022-05-27 22:18:56 +0200

    added amqp wrapper, rewrote ticketInquiry event to use event wrapper - buggy/pointless (currently still creates a new connection every time an event is sent)

--- smartkita-backend/events/stadtbus/sendTicketInquiry.js
+++ smartkita-backend/events/stadtbus/sendTicketInquiry.js
@@ -1,31 +1,19 @@
-const amqp = require('amqplib/callback_api');
+const amqp = require('../../lib/amqp');
 const {ajv} = require('../validation'); // destructuring: call specific variable (including functions) exported from the required module
 
 const rabbitMQUsername = process.env.rabbitMQUsername
 const rabbitMQPassword = process.env.rabbitMQPassword
 const rabbitMQserverURL = process.env.rabbitMQserverURL
 
-exports.sendTicketInquiry = async (req, res) => {
-    amqp.connect(`amqp://${rabbitMQUsername}:${rabbitMQPassword}@${rabbitMQserverURL}`, function(connectError, connection){
-        if (connectError) {
-            throw connectError
-        }
-        connection.createChannel(function (channelError, channel) {
-            if (channelError) {
-                throw channelError
-            }
+const routingKey = "private.kita"
 
+exports.sendTicketInquiry = async (req, res) => {
             // schema validation
             const validate = ajv.getSchema('stadtbus_sendTicketInquiry')
             if (validate(req.body)) {
-
-                channel.publish('events', "private.kita", Buffer.from(JSON.stringify((req.body))))
-                console.log(`RabbitMQ: sent event ${req.body.event_id}`)
-                return res.status(200).send({error: false, msg: 'event successfully sent'})
+                await amqp.publish(routingKey, req.body)
             } else {
                 // report error
                 return res.status(400).send({error: true, msg: 'invalid ticket inquiry data'})
             }
-         })
-        })
-    }
+         }
--- smartkita-backend/lib/amqp.js
+++ smartkita-backend/lib/amqp.js
@@ -1,10 +1,24 @@
-const amqp = require('amqplib/callback_api')
+const rabbitMQUsername = process.env.rabbitMQUsername
+const rabbitMQPassword = process.env.rabbitMQPassword
+const rabbitMQserverURL = process.env.rabbitMQserverURL
 
-amqp.connect(`amqp://${rabbitMQUsername}:${rabbitMQPassword}@${rabbitMQserverURL}`, { keepAlive: true }, function(connectError, connection) {
-    if (connectError) {
-        throw connectError
+const amqp = require('amqplib/callback_api.js')
+
+ class RabbitMQWrapper {
+    static #connectionString = `amqp://${rabbitMQUsername}:${rabbitMQPassword}@${rabbitMQserverURL}`;
+
+    static async publish(routingKey, body) {
+        console.log(body);
+        console.log(`RabbitMQ: attempting to send event ${body.event_name}`);
+        amqp.connect(RabbitMQWrapper.#connectionString, { keepAlive: true }, (connectError, connection) => {
+            if (connectError) { throw connectError; }
+            connection.createChannel((channelError, channel) => {
+                if (channelError) { throw channelError; }
+                channel.publish('events', routingKey, Buffer.from(JSON.stringify(body)));
+                console.log(`RabbitMQ: sent event ${body.event_name}`);
+            });
+        });
     }
-    console.log("AMQP connected successfully")
-})
+}
 
-module.exports = amqp;
+module.exports = RabbitMQWrapper;
