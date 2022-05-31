424cfd5a5222d24174637da771b04b8d9220f574
M <82416283+max-hertz@users.noreply.github.com>
2022-05-31 22:03:28 +0200

    Fixed frontend path

--- smartkita-backend/app.js
+++ smartkita-backend/app.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, "../smartkita-frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // app.get("*", async (req, res) =>{
   // res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
