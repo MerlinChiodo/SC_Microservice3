5eb49bd08a26236e45b2a6075dd38bec440f840a
M <82416283+max-hertz@users.noreply.github.com>
2022-06-05 13:55:52 +0200

    fixed typo

--- smartkita-backend/app.js
+++ smartkita-backend/app.js
@@ -65,8 +65,6 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-receive
-
 // custom middleware (err argument) => must be defined last in order
 // error handler
 app.use(function(err, req, res, next) {
