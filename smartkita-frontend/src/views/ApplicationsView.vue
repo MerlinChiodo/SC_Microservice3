28c53b761858d7abd2e69e7a7333b01c5cd07ab3
M <82416283+max-hertz@users.noreply.github.com>
2022-06-14 17:47:50 +0200

    added context menu to applications table

--- smartkita-frontend/src/views/ApplicationsView.vue
+++ smartkita-frontend/src/views/ApplicationsView.vue
@@ -11,6 +11,9 @@
       stripedRows
       :paginator="true"
       :rows="12"
+      contextMenu
+      v-model:contextMenuSelection="selectedApplication"
+      @rowContextmenu="onRowContextMenu"
     >
       <Column field="id_antrag" header="ID Antrag"></Column>
       <Column field="id_kind" header="ID Kind"></Column>
@@ -20,6 +23,8 @@
       <Column field="datum" header="Gestellt am"></Column>
       <Column field="prioritaet" header="Priorität"></Column>
       <Column field="status" header="Status"></Column>
+
+      <ContextMenu :model="menuModel" ref="cm" />
     </DataTable>
   </div>
 </template>
@@ -35,6 +40,33 @@ export default {
   data() {
     return {
       applicationsList: null,
+      selectedApplication: null,
+      menuModel: [
+        { label: "Details", icon: "pi pi-fw pi-search" },
+        {
+          label: "Status",
+          icon: "pi pi-align-justify",
+          items: [
+            {
+              label: "Annehmen",
+              icon: "pi pi-check-circle",
+              command: () => this.acceptApplication(this.selectedApplication)
+            },
+            {
+              label: "Unvollständig",
+              icon: "pi pi-exclamation-circle",
+              command: () => this.incompleteApplication(this.selectedApplication)
+            },            {
+              label: "Ablehnen",
+              icon: "pi pi-times-circle",
+              command: () => this.denyApplication(this.selectedApplication)
+            }
+            /*            {
+              separator:true
+            },*/
+          ],
+        },
+      ],
     };
   },
   computed: {
@@ -45,12 +77,25 @@ export default {
   },
   methods: {
     async getApplicationsList() {
-      const response = await fetch(
-        this.apiUrl + "applications/all"
-      );
+      const response = await fetch(this.apiUrl + "applications/all");
       const data = await response.json();
       this.applicationsList = data;
     },
+    onRowContextMenu(event) {
+      this.$refs.cm.show(event.originalEvent);
+    },
+    acceptApplication(application) {
+      application.status = "ANGENOMMEN"
+      console.log(application, "akzeptiert")
+    },
+    incompleteApplication(application) {
+      application.status = "UNVOLLSTAENDIG"
+      console.log(application, "als unvollständig markiert")
+    },
+    denyApplication(application) {
+      application.status = "ABGELEHNT"
+      console.log(application, "abgelehnt")
+    }
   },
 };
 </script>
