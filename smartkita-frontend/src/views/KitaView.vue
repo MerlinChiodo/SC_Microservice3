fed77284f2330bfc242261af2dcaf8dfeb40eae5
M <82416283+max-hertz@users.noreply.github.com>
2022-06-19 15:40:35 +0200

    added navigation from kita list to kita detail view

--- smartkita-frontend/src/views/KitaView.vue
+++ smartkita-frontend/src/views/KitaView.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="card">
-  <h1>KitaView</h1>
+    <h1>KitaView</h1>
+    {{ this.$route.params }}
+    <h3>Daten</h3>
+    {{ this.kitaData }}
   </div>
 </template>
+
+<script>
+export default {
+  beforeMount() {
+    this.getKitaData(this.$route.params.kitaId);
+  },
+  name: "KitaView",
+  inject: ["apiUrl"],
+  props: [],
+  data() {
+    return {
+      kitaData: null,
+    };
+  },
+  methods: {
+    async getKitaData(id_einrichtung) {
+      const response = await fetch(
+        this.apiUrl + "kitas/?id=" + id_einrichtung,
+        {
+          method: "GET",
+        }
+      );
+      const data = await response.json();
+      this.kitaData = data;
+    },
+  },
+};
+</script>
--- smartkita-frontend/src/views/SearchKitasView.vue
+++ smartkita-frontend/src/views/SearchKitasView.vue
@@ -8,6 +8,9 @@
       :rows="12"
       v-model:filters="filters"
       :globalFilterFields="['name', 'traeger', 'traeger_kategorie', 'plz']"
+      contextMenu
+      v-model:contextMenuSelection="selectedKita"
+      @rowContextmenu="onRowContextMenu"
     >
       <template #header>
         <div class="flex justify-content-center align-items-center">
@@ -31,7 +34,7 @@
       <Column field="plz" header="PLZ" :sortable="true"></Column>
       <Column field="email" header="E-Mail"></Column>
       <Column field="url_website" header="Website"></Column>
-      <Column
+      <!--      <Column
         headerStyle="width: 9rem; text-align: center"
         bodyStyle="text-align: center; overflow: visible"
       >
@@ -43,17 +46,17 @@
             style="margin-left: 0.5rem"
           ></Button>
         </template>
-      </Column>
+      </Column>-->
+      <ContextMenu :model="menuModel" ref="cm" />
     </DataTable>
   </div>
 </template>
 
 <script>
-import {FilterMatchMode} from 'primevue/api';
+import { FilterMatchMode } from "primevue/api";
 export default {
   beforeMount() {
     this.getKitasList();
-    console.log(this.kitasList);
   },
   name: "SearchKitasView",
   inject: ["apiUrl"],
@@ -61,16 +64,36 @@ export default {
   data() {
     return {
       kitasList: null,
+      selectedKita: null,
+      menuModel: [
+        {
+          label: "Vormerken",
+          icon: "pi pi-bookmark",
+          command: () => this.resetApplication(this.selectedKita),
+        },
+        {
+          label: "Detailansicht",
+          icon: "pi pi-angle-right",
+          command: () => this.toKitaView(this.selectedKita.id_einrichtung),
+        },
+      ],
       filters: {
         global: { value: null, matchMode: FilterMatchMode.CONTAINS },
       },
     };
   },
   methods: {
+    onRowContextMenu(event) {
+      this.$refs.cm.show(event.originalEvent);
+    },
     async getKitasList() {
       const response = await fetch(this.apiUrl + "kitas/all");
       const data = await response.json();
       this.kitasList = data;
+    },
+    toKitaView(kitaId) {
+      console.log(kitaId);
+      this.$router.push({ name: "kita", params: { kitaId } });
     },
   },
 };
@@ -96,6 +119,8 @@ export default {
 
 .p-input-icon-left > .p-inputtext:enabled:focus {
   border-color: var(--buttonColor);
-  box-shadow: inset 0 0 0 1px var(--buttonColor), inset 0 0 0 1px var(--buttonColor), inset 0 0 0 1px var(--buttonColor), inset 0 0 0 1px var(--buttonColor);
+  box-shadow: inset 0 0 0 1px var(--buttonColor),
+    inset 0 0 0 1px var(--buttonColor), inset 0 0 0 1px var(--buttonColor),
+    inset 0 0 0 1px var(--buttonColor);
 }
 </style>
