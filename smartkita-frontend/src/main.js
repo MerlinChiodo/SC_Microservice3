5ae90a24088a58c2cbd2ffe5f3eb89e45ff7e2f6
M <82416283+max-hertz@users.noreply.github.com>
2022-06-20 14:56:14 +0200

    prepared for PR

--- smartkita-frontend/src/main.js
+++ smartkita-frontend/src/main.js
@@ -35,8 +35,8 @@ app.use(router);
 app.use(PrimeVue);
 
 // constants
-app.provide("apiUrl", "http://localhost:3001/api/");
-// app.provide("apiUrl", "http://vps2290194.fastwebserver.de:9730/api/");
+// app.provide("apiUrl", "http://localhost:3001/api/");
+app.provide("apiUrl", "http://vps2290194.fastwebserver.de:9730/api/");
 
 app.provide("bbUrl", "http://vps2290194.fastwebserver.de:9710/api");
 
