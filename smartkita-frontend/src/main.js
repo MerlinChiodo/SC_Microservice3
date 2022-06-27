a09ed27b9f227ab535723dd1dae4d9d784e09aa6
M <82416283+max-hertz@users.noreply.github.com>
2022-06-27 14:45:32 +0200

    url fix

--- smartkita-frontend/src/main.js
+++ smartkita-frontend/src/main.js
@@ -44,6 +44,8 @@ app.provide("homeUrl", "http://localhost:3000/");
 /*app.provide("apiUrl", "http://" + location.host + "/api/");
 app.provide("homeUrl", "http://vps2290194.fastwebserver.de:9730/");*/
 
+//app.provide("homeUrl", "http://localhost:3001/");
+
 // other service URLs
 app.provide("bbUrl", "http://vps2290194.fastwebserver.de:9710/api");
 app.provide("authUrl", "http://auth.smartcityproject.net:8080/");
