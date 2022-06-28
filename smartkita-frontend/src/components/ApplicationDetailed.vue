696b59aabac5e464ac9a5919cfbba9be5474e0e4
M <82416283+max-hertz@users.noreply.github.com>
2022-06-28 15:48:24 +0200

    filled ApplicationDetailed with data, added buttons to handle applications

--- smartkita-frontend/src/components/ApplicationDetailed.vue
+++ smartkita-frontend/src/components/ApplicationDetailed.vue
@@ -7,7 +7,140 @@
     <p>EZB: {{ this.parentData }}</p>
     <p>Kind: {{ this.childData }}</p>
   </div>
-  <Button @click="$emit('backToApplList')">Zurück</Button>
+  <div
+    class="applicationDetailedContainer"
+    v-if="this.selectedApplicationData && this.parentData && this.childData"
+  >
+    <div class="surface-card p-4 shadow-2 border-round-md">
+      <div class="flex justify-content-between flex-wrap">
+        <div class="flex align-items-center justify-content-start w-8rem"></div>
+        <div
+          class="text-3xl font-medium text-900 mb-3 flex align-items-center justify-content-center"
+        >
+          <span style="font-weight: bold"
+            >Antrag #{{ this.selectedApplicationData.id_antrag }}</span
+          >
+        </div>
+        <div class="flex align-items-center justify-content-end w-8rem">
+          <Button
+            type="button"
+            icon="pi pi-backward"
+            style="margin-left: 3px"
+            @click="$emit('backToApplList')"
+          ></Button>
+        </div>
+      </div>
+      <div class="font-medium text-500 mb-3">
+        {{ this.selectedApplicationData.status }}
+      </div>
+      <div class="grid p-fluid" style="min-height: 150px">
+        <div class="col-12 md:col-4">
+          <div
+            v-if="parentData"
+            class="border-2 border-round-md surface-border flex flex-column align-items-center justify-content-center h-full"
+          >
+            <h4>Erziehungsberechtigte*r</h4>
+            <span>{{ parentData.firstname + " " + parentData.lastname }}</span>
+            Geschlecht: {{ this.parentData.gender }}
+            <span>{{
+              "geb. " +
+              new Date(parentData.birthdate).toISOString().split("T")[0]
+            }}</span>
+            <h5>Kontakt</h5>
+            <span>{{
+              parentData.address.street + " " + parentData.address.housenumber
+            }}</span>
+            <span>{{
+              parentData.address.city_code + " " + parentData.address.city
+            }}</span>
+            <p>{{ parentData.email }}</p>
+          </div>
+        </div>
+
+        <div class="col-12 md:col-4">
+          <div
+            v-if="childData"
+            class="border-2 border-round-md surface-border flex flex-column align-items-center justify-content-center h-full"
+          >
+            <div class="flex flex-wrap">
+              <div
+                class="flex align-items-center justify-content-center"
+                style="font-weight: bold"
+              >
+                <h4>Kind</h4>
+              </div>
+            </div>
+            <div v-if="childData">
+              <span>{{ childData.firstname + " " + childData.lastname }}</span
+              ><br />
+              <span>Geschlecht: {{ this.parentData.gender }}</span
+              ><br />
+              <span>{{
+                "geb. " +
+                new Date(childData.birthdate).toISOString().split("T")[0]
+              }}</span>
+              <h5>Wohnort</h5>
+              <span>{{
+                childData.address.street + " " + childData.address.housenumber
+              }}</span
+              ><br />
+              <span>{{
+                childData.address.city_code + " " + childData.address.city
+              }}</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 md:col-4">
+          <div
+            v-if="selectedApplicationData"
+            class="border-2 border-round-md surface-border flex flex-column align-items-center justify-content-center h-full"
+          >
+            <h4>Antragsdetails</h4>
+            <span
+              >Einrichtung:
+              {{ this.selectedApplicationData.id_einrichtung }}</span
+            >
+            <span
+              >Betreuungsstunden:
+              {{ this.selectedApplicationData.betreuungsstunden }}</span
+            >
+          </div>
+        </div>
+        <div class="col-12 md:col-8">
+          <div
+            v-if="selectedApplicationData"
+            class="border-2 border-round-md surface-border flex flex-column align-items-center justify-content-center h-full"
+          >
+            <h4>Bemerkung</h4>
+            <p>{{ this.selectedApplicationData.bemerkung }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="p-buttonset" style="margin-top: 16px">
+        <Button
+          label="Annehmen"
+          icon="pi pi-check-circle"
+          iconPos="left"
+          @click="$emit('changeStatus', this.selectedApplication, 'accept')"
+        ></Button>
+        <Button
+          label="Unvollständig"
+          icon="pi pi-exclamation-circle"
+          @click="$emit('changeStatus', this.selectedApplication, 'accept')"
+        ></Button>
+        <Button
+          label="Ablehnen"
+          icon="pi pi-times-circle"
+          @click="$emit('changeStatus', this.selectedApplication, 'accept')"
+        ></Button>
+        <Button
+          label="Eingegangen"
+          icon="pi pi-undo"
+          @click="$emit('changeStatus', this.selectedApplication, 'accept')"
+        ></Button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -31,7 +164,7 @@ export default {
   emits: ["backToApplList"],
   data() {
     return {
-      debug: true,
+      debug: false,
       selectedApplicationData: null,
       parentData: null,
       childData: null,
@@ -61,4 +194,23 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.applicationDetailedContainer {
+  margin: auto;
+  margin-top: 2rem;
+  max-width: 80rem;
+}
+.p-button,
+.p-button-raised {
+  background-color: var(--buttonColor);
+}
+
+.p-button:hover {
+  background-color: var(--buttonHoverColor) !important;
+}
+
+.p-button:active,
+.p-button:focus {
+  background-color: var(--buttonDarkerColor) !important;
+}
+</style>
